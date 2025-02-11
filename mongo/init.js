fdea68106b9f2de6672aa1001f5a9919714f23d0
Egor Tarasov <pavluchenk0@yandex.ru>
2025-02-11 06:57:59 +0100

    mongo rs0 workaround (#18)

--- mongo/init.js
+++ mongo/init.js
@@ -1,6 +1,16 @@
-rs.initiate({
-    _id: "rs0",
-    members: [
-        { _id: 0, host: "localhost:27017" }
-    ]
-});
+ensureInitialized();
+
+function ensureInitialized() {
+    if (rs.status().ok === 1) {
+        print("Replica set already initialized");
+        return;
+    }
+
+    print("Initializing replica set");
+    rs.initiate({
+        _id: "rs0",
+        members: [
+            { _id: 0, host: "localhost:27017" }
+        ]
+    });
+}
