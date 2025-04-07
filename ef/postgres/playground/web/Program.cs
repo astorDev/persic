d589ac18507329c49f57a463901d2aed115a92c0
Egor Tarasov <pavluchenk0@yandex.ru>
2025-04-07 15:39:55 +0300

    postgres article intro outro

--- ef/postgres/playground/web/Program.cs
+++ ef/postgres/playground/web/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Persic;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddPostgres<Db>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+await using var scope = app.Services.CreateAsyncScope();
+var db = scope.ServiceProvider.GetRequiredService<Db>();
+var canConnect = await db.Database.CanConnectAsync();
+app.Logger.LogInformation("Can connect to database: {CanConnect}", canConnect);
 
 app.Run();
+
+public class Db(DbContextOptions<Db> options) : DbContext(options) {
+}
