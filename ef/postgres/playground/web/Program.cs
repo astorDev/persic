23e7f1f86d9a8e3362bf1142dff2a3bb4ad0d9c3
Egor Tarasov <vosarat1995+1@gmail.com>
2025-04-09 10:52:27 +0300

    postgres article assets (#29)

--- ef/postgres/playground/web/Program.cs
+++ ef/postgres/playground/web/Program.cs
@@ -1,18 +1,248 @@
+using Confi;
 using Microsoft.EntityFrameworkCore;
 using Persic;
 
-var builder = WebApplication.CreateBuilder(args);
+await VFinal.Run(args);
 
-builder.Services.AddPostgres<Db>();
+public class V0
+{
+    public static async Task Run(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+        builder.Logging.AddSimpleConsole(c => c.SingleLine = true);
 
-await using var scope = app.Services.CreateAsyncScope();
-var db = scope.ServiceProvider.GetRequiredService<Db>();
-var canConnect = await db.Database.CanConnectAsync();
-app.Logger.LogInformation("Can connect to database: {CanConnect}", canConnect);
+        builder.Services.AddDbContext<Db>((sp, options) =>
+        {
+            options.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=playground");
+        });
 
-app.Run();
+        var app = builder.Build();
 
-public class Db(DbContextOptions<Db> options) : DbContext(options) {
+        await using var scope = app.Services.CreateAsyncScope();
+        var db = scope.ServiceProvider.GetRequiredService<Db>();
+        var canConnect = await db.Database.CanConnectAsync();
+        app.Logger.LogInformation("Can connect to database: {CanConnect}", canConnect);
+    }
+
+    public class Db(DbContextOptions<Db> options) : DbContext(options) {
+    }
+}
+
+public class V1
+{
+    public static async Task Run(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Logging.AddSimpleConsole(c => c.SingleLine = true);
+
+        builder.Services.AddDbContext<Db>((sp, options) =>
+        {
+            options.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=playground");
+        });
+
+        var app = builder.Build();
+
+        await using var scope = app.Services.CreateAsyncScope();
+        var db = scope.ServiceProvider.GetRequiredService<Db>();
+        var canConnect = await db.Database.CanConnectAsync();
+        app.Logger.LogInformation("Can connect to database: {CanConnect}", canConnect);
+
+        await db.Database.EnsureDeletedAsync();
+        await db.Database.EnsureCreatedAsync();
+
+        db.Add(new Record { Name = "Test" });
+
+        await db.SaveChangesAsync();
+
+        var records = await db.Records.ToListAsync();
+    }
+
+    public class Db(DbContextOptions<Db> options) : DbContext(options) {
+        public DbSet<Record> Records { get; set; } = null!;
+    }
+
+    public class Record
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
+
+public class V2
+{
+    public static async Task Run(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Logging.AddSimpleConsole(c => c.SingleLine = true);
+
+        builder.Services.AddDbContext<Db>((sp, options) =>
+        {
+            options.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=playground")
+                .UseSnakeCaseNamingConvention();
+        });
+
+        var app = builder.Build();
+
+        await using var scope = app.Services.CreateAsyncScope();
+        var db = scope.ServiceProvider.GetRequiredService<Db>();
+        var canConnect = await db.Database.CanConnectAsync();
+        app.Logger.LogInformation("Can connect to database: {CanConnect}", canConnect);
+
+        await db.Database.EnsureDeletedAsync();
+        await db.Database.EnsureCreatedAsync();
+
+        db.Add(new Record { Name = "Test" });
+
+        await db.SaveChangesAsync();
+
+        var records = await db.Records.ToListAsync();
+    }
+
+    public class Db(DbContextOptions<Db> options) : DbContext(options) {
+        public DbSet<Record> Records { get; set; } = null!;
+    }
+
+    public class Record
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
+
+public class V3
+{
+    public static async Task Run(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Logging.AddSimpleConsole(c => c.SingleLine = true);
+
+        builder.Services.AddDbContext<Db>((sp, options) =>
+        {
+            var connectionString = builder.Configuration.GetConnectionString("Postgres");
+
+            options.UseNpgsql(connectionString)
+                .UseSnakeCaseNamingConvention();
+        });
+
+        var app = builder.Build();
+
+        await using var scope = app.Services.CreateAsyncScope();
+        var db = scope.ServiceProvider.GetRequiredService<Db>();
+        var canConnect = await db.Database.CanConnectAsync();
+        app.Logger.LogInformation("Can connect to database: {CanConnect}", canConnect);
+
+        await db.Database.EnsureDeletedAsync();
+        await db.Database.EnsureCreatedAsync();
+
+        db.Add(new Record { Name = "Test" });
+
+        await db.SaveChangesAsync();
+
+        var records = await db.Records.ToListAsync();
+    }
+
+    public class Db(DbContextOptions<Db> options) : DbContext(options) {
+        public DbSet<Record> Records { get; set; } = null!;
+    }
+
+    public class Record
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
+
+public class V4
+{
+    public static async Task Run(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Logging.AddSimpleConsole(c => c.SingleLine = true);
+
+        builder.Services.AddPostgreDbContext<Db>();
+
+        var app = builder.Build();
+
+        await using var scope = app.Services.CreateAsyncScope();
+        var db = scope.ServiceProvider.GetRequiredService<Db>();
+        var canConnect = await db.Database.CanConnectAsync();
+        app.Logger.LogInformation("Can connect to database: {CanConnect}", canConnect);
+
+        await db.Database.EnsureDeletedAsync();
+        await db.Database.EnsureCreatedAsync();
+
+        db.Add(new Record { Name = "Test" });
+
+        await db.SaveChangesAsync();
+
+        var records = await db.Records.ToListAsync();
+    }
+
+    public class Db(DbContextOptions<Db> options) : DbContext(options) {
+        public DbSet<Record> Records { get; set; } = null!;
+    }
+
+    public class Record
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddPostgreDbContext<TContext>(this IServiceCollection services, string configurationPath = "ConnectionStrings:Postgres")
+        where TContext : DbContext
+    {
+        return services.AddDbContext<TContext>((sp, options) => {
+            var config = sp.GetRequiredService<IConfiguration>();
+            var connectionString = config.GetRequiredValue(configurationPath);
+
+            options.UseNpgsql(connectionString)
+                .UseSnakeCaseNamingConvention();
+        });
+    }
+}
+
+public class VFinal
+{
+    public static async Task Run(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Logging.AddSimpleConsole(c => c.SingleLine = true);
+
+        builder.Services.AddPostgres<Db>();
+
+        var app = builder.Build();
+
+        await using var scope = app.Services.CreateAsyncScope();
+        var db = scope.ServiceProvider.GetRequiredService<Db>();
+        var canConnect = await db.Database.CanConnectAsync();
+        app.Logger.LogInformation("Can connect to database: {CanConnect}", canConnect);
+
+        await db.Database.EnsureDeletedAsync();
+        await db.Database.EnsureCreatedAsync();
+
+        db.Add(new Record { Name = "Test" });
+
+        await db.SaveChangesAsync();
+
+        var records = await db.Records.ToListAsync();
+    }
+
+    public class Db(DbContextOptions<Db> options) : DbContext(options) {
+        public DbSet<Record> Records { get; set; } = null!;
+    }
+
+    public class Record
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
 }
