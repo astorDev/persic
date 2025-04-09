8e6b1bd8bda1a5df8858cf2538430ee82de08a06
Egor Tarasov <pavluchenk0@yandex.ru>
2025-04-09 10:52:14 +0300

    v final and co

--- ef/postgres/playground/web/Program.cs
+++ ef/postgres/playground/web/Program.cs
@@ -1,11 +1,12 @@
 using Confi;
 using Microsoft.EntityFrameworkCore;
+using Persic;
 
-await V4.Main(args);
+await VFinal.Run(args);
 
 public class V0
 {
-    public static async Task Main(string[] args)
+    public static async Task Run(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,6 @@ public class V0
 
         builder.Services.AddDbContext<Db>((sp, options) =>
         {
-            
             options.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=playground");
         });
 
@@ -31,7 +31,7 @@ public class V0
 
 public class V1
 {
-    public static async Task Main(string[] args)
+    public static async Task Run(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
@@ -72,7 +72,7 @@ public class V1
 
 public class V2
 {
-    public static async Task Main(string[] args)
+    public static async Task Run(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
@@ -114,7 +114,7 @@ public class V2
 
 public class V3
 {
-    public static async Task Main(string[] args)
+    public static async Task Run(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
@@ -158,7 +158,7 @@ public class V3
 
 public class V4
 {
-    public static async Task Main(string[] args)
+    public static async Task Run(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
@@ -206,5 +206,43 @@ public static class ServiceCollectionExtensions
             options.UseNpgsql(connectionString)
                 .UseSnakeCaseNamingConvention();
         });
+    }
+}
+
+public class VFinal
+{
+    public static async Task Run(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Logging.AddSimpleConsole(c => c.SingleLine = true);
+
+        builder.Services.AddPostgres<Db>();
+
+        var app = builder.Build();
+
+        await using var scope = app.Services.CreateAsyncScope();
+        var db = scope.ServiceProvider.GetRequiredService<Db>();
+        var canConnect = await db.Database.CanConnectAsync();
+        app.Logger.LogInformation("Can connect to database: {CanConnect}", canConnect);
+
+        await db.Database.EnsureDeletedAsync();
+        await db.Database.EnsureCreatedAsync();
+
+        db.Add(new Record { Name = "Test" });
+
+        await db.SaveChangesAsync();
+
+        var records = await db.Records.ToListAsync();
+    }
+
+    public class Db(DbContextOptions<Db> options) : DbContext(options) {
+        public DbSet<Record> Records { get; set; } = null!;
+    }
+
+    public class Record
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
     }
 }
