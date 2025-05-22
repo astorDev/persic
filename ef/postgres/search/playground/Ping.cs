44ce4441b7febc8601ed472dae862c9e554da639
Egor Tarasov <pavluchenk0@yandex.ru>
2025-05-22 14:45:33 +0400

    ef/postgres/search inited (#37)

---
+++ ef/postgres/search/playground/Ping.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persic.EF.Postgres.Playground;
+
+[TestClass]
+public sealed class Ping
+{
+    [TestMethod]
+    public async Task Connected()
+    {
+        using var db = new Db();
+        await db.Database.OpenConnectionAsync();
+    }
+}
+
+public class Db : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseNpgsql("Host=localhost;Port=5631;Database=postgres_search;Username=postgres_search;Password=postgres_search")
+            .UseSnakeCaseNamingConvention();
+    }
+}
