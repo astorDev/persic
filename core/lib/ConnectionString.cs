61536db822b38ec7e5e57029a824234d9c1407ef
Egor Tarasov <vosarat1995@google.com>
2025-07-09 13:52:07 +0400

    Core: Inited (ConnectionString)

---
+++ core/lib/ConnectionString.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Persic.Core;
+
+public record ConnectionString(Dictionary<string, string> Parameters)
+{
+    public static ConnectionString Parse(string connectionString)
+    {
+        var parameters = connectionString
+            .Split(';')
+            .Select(p => p.Split('='))
+            .Where(p => p.Length == 2)
+            .ToDictionary(p => p[0], p => p[1]);
+
+        return new ConnectionString(parameters);
+    }
+}
+
+public static class MappingExtensions
+{
+    public static T To<T>(this ConnectionString connectionString) where T : class, new()
+    {
+        IEnumerable<KeyValuePair<string, string?>> parameters = connectionString.Parameters.Select(kvp => new KeyValuePair<string, string?>(kvp.Key, kvp.Value));
+
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(parameters)
+            .Build();
+
+        IServiceCollection services = new ServiceCollection();
+
+        services
+            .AddOptions<T>()
+            .Bind(configuration)
+            .ValidateDataAnnotations();
+
+        return services.BuildServiceProvider().GetRequiredService<IOptions<T>>().Value;
+    }
+}
